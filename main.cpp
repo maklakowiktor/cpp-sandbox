16f33b1a325dec5851f949e8f4fecd3fb86d3439
maklakowiktor <maklakowiktor@gmail.com>
2022-01-12 00:56:39 +0300

    ...

--- main.cpp
+++ main.cpp
@@ -4,9 +4,11 @@
 int main() 
 {
     setlocale(LC_ALL, "");
-    int a;
-    
+    int a, b {88};
+    a = 301;
+    b = 89;
     a = factorial(5);
+    std::cout << a << b << std::endl;
     std::cout << "Result: " << a << std::endl;
 
     return 0;
